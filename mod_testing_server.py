733f5c0f5e78b5b90e1fd587df1ee840f93a12b0
tobspr <tobias.springer1@googlemail.com>
2019-10-07 14:51:13 +0200

    Update sample mod

---
+++ mod_testing_server.py
@@ -0,0 +1,53 @@
+import SimpleHTTPServer
+
+
+class CORSHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def send_head(self):
+        path = self.translate_path(self.path)
+        f = None
+        if os.path.isdir(path):
+            if not self.path.endswith('/'):
+                # redirect browser - doing basically what apache does
+                self.send_response(301)
+                self.send_header("Location", self.path + "/")
+                self.end_headers()
+                return None
+            for index in "index.html", "index.htm":
+                index = os.path.join(path, index)
+                if os.path.exists(index):
+                    path = index
+                    break
+            else:
+                return self.list_directory(path)
+        ctype = self.guess_type(path)
+        try:
+            # Always read in binary mode. Opening files in text mode may cause
+            # newline translations, making the actual size of the content
+            # transmitted *less* than the content-length!
+            f = open(path, 'rb')
+        except IOError:
+            self.send_error(404, "File not found")
+            return None
+        self.send_response(200)
+        self.send_header("Content-type", ctype)
+        fs = os.fstat(f.fileno())
+        self.send_header("Content-Length", str(fs[6]))
+        self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.end_headers()
+        return f
+
+
+if __name__ == "__main__":
+    import os
+    import SocketServer
+
+    PORT = 8000
+    Handler = CORSHTTPRequestHandler
+    # For python 3
+    #Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+    httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+    print "serving at port", PORT
+    httpd.serve_forever()
