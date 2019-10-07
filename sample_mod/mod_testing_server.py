d8b58df89fefc4caf487c5199552f8389c86e256
xedre <xt.xedretaw@gmail.com>
2019-10-07 16:54:08 +0100

    changed SocketServer import for python 3

--- sample_mod/mod_testing_server.py
+++ sample_mod/mod_testing_server.py
@@ -1,4 +1,4 @@
-from http.server import SimpleHTTPServerHandler
+from http.server import SimpleHTTPRequestHandler
 
 
 class CORSHTTPRequestHandler(SimpleHTTPRequestHandler):
@@ -40,14 +40,14 @@ class CORSHTTPRequestHandler(SimpleHTTPRequestHandler):
 
 if __name__ == "__main__":
     import os
-    import SocketServer
+    import socketserver
 
     PORT = 8000
     Handler = CORSHTTPRequestHandler
     # For python 3
     #Handler = SimpleHTTPRequestHandler
 
-    httpd = SocketServer.TCPServer(("", PORT), Handler)
+    httpd = socketserver.TCPServer(("", PORT), Handler)
 
-    print("serving at port: ", str(PORT))
+    print("serving at port", str(PORT))
     httpd.serve_forever()
